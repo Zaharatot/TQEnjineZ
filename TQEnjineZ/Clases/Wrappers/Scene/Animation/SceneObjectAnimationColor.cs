b0958afba0a3cdcef74c550464b933b4768ddc74
Захар Максименко <Maksimenko_ZA@edu.bstu.ru>
2018-05-11 13:53:08 +0300

    Последний на сегодня патч
    Поправил чуть-чуть стили, дописал немного анимации.

---
+++ TQEnjineZ/Clases/Wrappers/Scene/Animation/SceneObjectAnimationColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TQEnjineZ.Clases.Wrappers.Scene.Animation
+{
+    /// <summary>
+    /// Информация, о параметрах анимации
+    /// Тип анимации - изменение цвета
+    /// </summary>
+    class SceneObjectAnimationColor
+    {
+    }
+}
--- TQEnjineZ/Clases/Wrappers/Scene/Style/SceneObjectStyle.cs
+++ TQEnjineZ/Clases/Wrappers/Scene/Style/SceneObjectStyle.cs
@@ -13,14 +13,68 @@ namespace TQEnjineZ.Clases.Wrappers.Scene.Style
     class SceneObjectStyle
     {
         /// <summary>
-        /// Параметры цвета стиля
+        /// Делегат события изменения стиля объекта сцены
         /// </summary>
-        public SceneObjectStyleColor Color { get; set; }
+        public delegate void EditStyleEvent();
+        /// <summary>
+        /// Ивент изменения цвета объекта сцены
+        /// </summary>
+        public event EditStyleEvent EditColor;
+        /// <summary>
+        /// Ивент изменения шрифта объекта сцены
+        /// </summary>
+        public event EditStyleEvent EditFont;
+
+        /// <summary>
+        /// Цвет стиля
+        /// </summary>
+        private SceneObjectStyleColor styleColor;
+        /// <summary>
+        /// Шрифт стиля
+        /// </summary>
+        private SceneObjectStyleFont styleFont;
+
+
+
+        /// <summary>
+        /// Публичные параметры цвета стиля
+        /// </summary>
+        public SceneObjectStyleColor Color
+        {
+            get
+            {
+                return styleColor;
+            }
+            set
+            {
+                //Присваиваем цвета стиля1
+                styleColor = value;
+                //Добавляем прослушку внутренних ивентов
+                Color.EditColor += Color_EditColor;
+                //Вызываем ивент
+                EditColor?.Invoke();
+            }
+        }
         
         /// <summary>
-        /// Параметры шрифта стиля
+        /// Публичные параметры шрифта стиля
         /// </summary>
-        public SceneObjectStyleFont Font { get; set; }
+        public SceneObjectStyleFont Font
+        {
+            get
+            {
+                return styleFont;
+            }
+            set
+            {
+                //Присваиваем цвета стиля1
+                styleFont = value;
+                //Добавляем прослушку внутренних ивентов
+                Font.EditFont += Font_EditFont;
+                //Вызываем ивент
+                EditFont?.Invoke();
+            }
+        }
 
         /// <summary>
         /// Инициализация объекта сцены дефолтными значениями
@@ -30,6 +84,27 @@ namespace TQEnjineZ.Clases.Wrappers.Scene.Style
             //Инициализируем дефолтные значения
             Color = new SceneObjectStyleColor();
             Font = new SceneObjectStyleFont();
+            //Добавляем прослушку внутренних ивентов
+            Color.EditColor += Color_EditColor;
+            Font.EditFont += Font_EditFont;
+        }
+
+        /// <summary>
+        /// ивент изменения шрифта
+        /// </summary>
+        private void Font_EditFont()
+        {
+            //Вызываем внешний ивент
+            EditFont?.Invoke();
+        }
+
+        /// <summary>
+        /// ивент изменения цвета
+        /// </summary>
+        private void Color_EditColor()
+        {
+            //Вызываем внешний ивент
+            EditColor?.Invoke();
         }
     }
 }
